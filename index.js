ba4af792827a73affed80022464cc3a7ea74a020
Aniksaha-ui <sahaanik1045@gmail.com>
2022-05-19 02:18:50 +0600

    mytodolist done

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fsfac.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dcmrw.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri);
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -20,19 +21,26 @@ const client = new MongoClient(uri, {
 function verifyJWT(req, res, next) {
   const authHeader = req.headers.authorization;
   if (!authHeader) {
-    return res.status(401).send({ message: "UnAuthorized access" });
+    return res.status(401).send({ message: "Unauthorized access" });
   }
+
   const token = authHeader.split(" ")[1];
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+  // console.log(token);
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
     if (err) {
-      return res.status(403).send({ message: "Forbidden access" });
+      return res.status(403).send({ message: "Forbidden Access" });
     }
+
     req.decoded = decoded;
-    next();
   });
+  next();
 }
 
 async function run() {
+  await client.connect();
+
+  const todoCollection = client.db("Todo").collection("listoftodo");
+  // console.log(todoCollection);
   try {
     app.post("/login", async (req, res) => {
       const user = req.body;
@@ -52,6 +60,30 @@ async function run() {
       console.log(accessToken);
       // console.log(user);
       res.send({ accessToken });
+    });
+
+    app.get("/todolist", async (req, res) => {
+      res.send("hello");
+    });
+
+    app.get("/mytodolist", verifyJWT, async (req, res) => {
+      const decodedEmail = req.decoded?.email;
+      const email = req.query.email;
+      if (email === decodedEmail) {
+        const query = { email: email };
+        const cursor = todoCollection.find(query);
+        const product = await cursor.toArray();
+        res.send(product);
+      } else {
+        res.status(403).send({ message: "forbidden access" });
+      }
+    });
+
+    app.delete("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productCollection.deleteOne(query);
+      res.send(result);
     });
   } finally {
   }
